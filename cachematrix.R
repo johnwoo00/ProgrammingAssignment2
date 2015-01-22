f3629604b426f1193f09dd04916020bd5fb941f8
johnwoo00 <johnwoo00@yahoo.com>
2015-01-21 21:00:47 -0500

    Update cachematrix.R
    Submission #1
    John H. Woo
    1/21/15

--- cachematrix.R
+++ cachematrix.R
@@ -7,17 +7,17 @@
 ## note: code based on "makeVector" example code.
 
 makeCacheMatrix <- function(x = matrix()) {
-     x_inv <- NULL
-     set <- function(y) {
-          x <<- y
-          x_inv <<- NULL
-     }
-     get <- function() x
-     setinv <- function(solve) x_inv <<- solve
-     getinv <- function() x_inv
-     list(set = set, get = get,
-          setinv = setinv,
-          getinv = getinv)
+        x_inv <- NULL
+        set <- function(y) {
+                x <<- y
+                x_inv <<- NULL
+        }
+        get <- function() x
+        setinv <- function(solve) x_inv <<- solve
+        getinv <- function() x_inv
+        list(set = set, get = get,
+                setinv = setinv,
+                getinv = getinv)
 }
 
 ## cacheSolve - This function computes the inverse of the special 
@@ -29,15 +29,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-     x_inv <- x$getinv()
-     if(!is.null(x_inv)) {
-          message("getting cached data")
-          return(x_inv)
-     }
-     data <- x$get()
-     x_inv <- solve(data, ...)
-     x$setinv(x_inv)
-     x_inv
+        x_inv <- x$getinv()
+        if(!is.null(x_inv)) {
+                message("getting cached data")
+                return(x_inv)
+        }
+        data <- x$get()
+        x_inv <- solve(data, ...)
+        x$setinv(x_inv)
+        x_inv
 }
 
 
